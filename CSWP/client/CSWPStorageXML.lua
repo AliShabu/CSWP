54c2eff75c692d7e22e2f164086ca88f92de7e72
Samake <samake03@gmail.com>
2013-09-26 22:18:51 +0200

    XML Handling
    -- implement first version of XML handling

--- CSWP/client/CSWPStorageXML.lua
+++ CSWP/client/CSWPStorageXML.lua
@@ -14,3 +14,69 @@
 	File: CSWPStorageXML.lua
 	Author: Sam@ke
 --]]
+
+
+function initXMLStorage()
+	local cswpSettings = cswpGetXMLFile()
+	
+	if (not cswpSettings) then
+		cswpSettings = xmlCreateFile("files/settings/settings.xml", "CSWP_Settings")   
+		if (cswpSettings) then
+			cswpSetXMLFile(cswpSettings)
+			saveXMLData("test", "test")
+		else
+			outputChatBox("ERROR: Could´nt create CSWP settings.xml! Use /debugscript 3 for more details!")
+		end
+	end
+end
+addEventHandler("onClientResourceStart", resourceRoot, initXMLStorage)
+
+
+function parseXMLData(nodeName)
+	local cswpSettings = cswpGetXMLFile() 
+	local node = xmlFindChild(cswpSettings, nodeName, 0)
+	local nodeValue = nil
+	
+	if (cswpSettings) then
+		if (node) then
+			nodeValue = xmlNodeGetValue(node)
+		else
+			outputChatBox("Warning: " .. nodeName .. " not found in settings.xml! Use /debugscript 3 for more details!")
+		end
+		
+		xmlUnloadFile(cswpSettings)
+	else
+		outputChatBox("ERROR: Settings.xml not found! Use /debugscript 3 for more details!")
+	end
+	
+	return nodeValue
+end
+
+
+function saveXMLData(nodeName, value)
+	if (nodeName) and (value) then
+		local cswpSettings = cswpGetXMLFile()
+		local node = xmlFindChild(cswpSettings, nodeName, 0)
+		
+		if (cswpSettings) then
+			if (node) then
+				xmlNodeSetValue(node, tostring(value))
+				xmlSaveFile(cswpSettings)  
+				xmlUnloadFile(cswpSettings)
+			else
+				xmlCreateChild(cswpSettings, tostring(nodeName))
+				node = xmlFindChild(cswpSettings, nodeName, 0)
+				
+				if (node) then
+					xmlNodeSetValue(node, tostring(value))
+					xmlSaveFile(cswpSettings)  
+					xmlUnloadFile(cswpSettings)
+				else
+					outputChatBox("ERROR: Could´nt be saved! Use /debugscript 3 for more details!")
+				end
+			end
+		else
+			outputChatBox("ERROR: Settings.xml not found! Use /debugscript 3 for more details!")
+		end	
+	end
+end
--- CSWP/client/CSWPVariablesC.lua
+++ CSWP/client/CSWPVariablesC.lua
@@ -14,3 +14,17 @@
 	File: CSWPVariablesC.lua
 	Author: Sam@ke
 --]]
+
+local cswpSettingsFile = xmlLoadFile("files/settings/settings.xml")
+
+
+-- // XML Handling // -- 
+function cswpSetXMLFile(file)
+	if (file) then
+		cswpSettingsFile = file
+	end
+end
+
+function cswpGetXMLFile()
+	return cswpSettingsFile
+end
